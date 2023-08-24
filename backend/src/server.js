4ebb4c580515b0c0b85cfe1f91f55901af09eefc
João Felipe Zini <joaofelipezini@gmail.com>
2023-08-24 09:25:26 -0300

    feat: connection works

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,20 @@
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize({
+  dialect: 'mysql',
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  host: process.env.MYSQL_HOSTNAME,
+  port: process.env.MYSQL_PORT,
+  database: process.env.MYSQL_DATABASE || 'StoreManager',
+})
+
+sequelize.authenticate()
+  .then(() => console.log('Conectado ao banco de dados com sucesso!'))
+  .catch((err) => {
+    console.error('Não foi possível conectar ao banco de dados:', err);
+    process.exit(1);
+  });
+
 const app = require('./app');
 
 const PORT = process.env.PORT || 3001;
